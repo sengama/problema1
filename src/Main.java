66889480d0455d64f9d6002e4a1cc0e9066303a8
sengama <146013628+sengama@users.noreply.github.com>
2023-09-28 13:52:38 +0300

    problema 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+
+        var var = new Variabless();
+
+            System.out.println("Current World Population - " + var.population);
+            System.out.println("Cars Produced This Year - " + var.total);
+            System.out.println("First Letter In Alphabet - " + var.c1);
+            System.out.println("Java Is Amazing - " + var.voucherPurchased);
+            System.out.println("PI - " + var.average);
+
+    }
+}
---
+++ src/Variabless.java
@@ -0,0 +1,9 @@
+public class Variabless {
+
+       long population = 7811080815L;
+       int total = 55235358;
+       char c1 = 'A';
+       boolean voucherPurchased = true;
+       double average = 3.14159;
+
+}
